e7a7b9a96feebb8b3e93fe4750cb791d04da5434
lordslair <rvandepo+github@gmail.com>
2018-08-03 21:02:44 +0000

    Added /lib/

---
+++ docker/lib/GLB/GLAPI.pm
@@ -0,0 +1,79 @@
+package GLB::GLAPI;
+
+use LWP;
+use YAML::Tiny;
+
+sub GetClanEquipement
+{
+    my $glfile = shift;
+    my $glyaml = YAML::Tiny->read( $glfile );
+
+    my $browser = new LWP::UserAgent;
+    my $request = new HTTP::Request( GET => "http://ie.gobland.fr/IE_ClanEquipement?id=$glyaml->[0]{gl_user}&passwd=$glyaml->[0]{gl_api_key}" );
+    my $headers = $request->headers();
+       $headers->header( 'User-Agent','Mozilla/5.0 (compatible; Konqueror/3.4; Linux) KHTML/3.4.2 (like Gecko)');
+       $headers->header( 'Accept', 'text/html, image/jpeg, image/png, text/*, image/*, */*');
+       $headers->header( 'Accept-Encoding','x-gzip, x-deflate, gzip, deflate');
+       $headers->header( 'Accept-Charset', 'iso-8859-15, utf-8;q=0.5, *;q=0.5');
+       $headers->header( 'Accept-Language', 'fr, en');
+       $headers->header( 'Referer', 'http://ie.gobland.fr');
+    my $response = $browser->request($request);
+
+    if ($response->is_success)
+    {
+        foreach my $line (split(/\n/,$response->content))
+        {
+            chomp ($line);
+            #"IdGob";"Id";"Type";"Identifie";"Nom";"Magie";"Desc";"Poids";"Taille";"Qualite";"Utilise";"Matiere"
+            if ( $line =~ /^"(\d*)";"(\d*)";"([^"]*)";"(\w*)";"([^"]*)";"(\w*)";"([^"]*)";"(\d*)";"(\d*)";"(\d*)";"(\w*)";"(\w*)"/ )
+            {
+                if ( $11 eq 'FAUX' ) { $equipe = 'NonEquipe' } else { $equipe = 'Equipe'}
+                $INVENTAIRE{$1}{$equipe}{$2}{'Type'}      = $3;
+                $INVENTAIRE{$1}{$equipe}{$2}{'Identifie'} = $4;
+                $INVENTAIRE{$1}{$equipe}{$2}{'Nom'}       = Encode::decode_utf8($5);
+                $INVENTAIRE{$1}{$equipe}{$2}{'Magie'}     = $6;
+                $INVENTAIRE{$1}{$equipe}{$2}{'Desc'}      = $7;
+                $INVENTAIRE{$1}{$equipe}{$2}{'Poids'}     = $8;
+                $INVENTAIRE{$1}{$equipe}{$2}{'Taille'}    = $9;
+                $INVENTAIRE{$1}{$equipe}{$2}{'Qualite'}   = $10;
+                $INVENTAIRE{$1}{$equipe}{$2}{'Utilise'}   = $11;
+                $INVENTAIRE{$1}{$equipe}{$2}{'Matiere'}   = $12;
+            }
+        }
+        return \%INVENTAIRE;
+    }
+}
+
+sub GetClanMembres
+{
+    my $glfile = shift;
+    my $glyaml = YAML::Tiny->read( $glfile );
+    my %MEMBRES;
+
+    my $browser = new LWP::UserAgent;
+    my $request = new HTTP::Request( GET => "http://ie.gobland.fr/IE_ClanMembres?id=$glyaml->[0]{gl_user}&passwd=$glyaml->[0]{gl_api_key}" );
+    my $headers = $request->headers();
+       $headers->header( 'User-Agent','Mozilla/5.0 (compatible; Konqueror/3.4; Linux) KHTML/3.4.2 (like Gecko)');
+       $headers->header( 'Accept', 'text/html, image/jpeg, image/png, text/*, image/*, */*');
+       $headers->header( 'Accept-Encoding','x-gzip, x-deflate, gzip, deflate');
+       $headers->header( 'Accept-Charset', 'iso-8859-15, utf-8;q=0.5, *;q=0.5');
+       $headers->header( 'Accept-Language', 'fr, en');
+       $headers->header( 'Referer', 'http://ie.gobland.fr');
+    my $response = $browser->request($request);
+
+    if ($response->is_success)
+    {
+        foreach my $line (split(/\n/,$response->content))
+        {
+            chomp ($line);
+            #"Id";"Nom";"Race";"Tribu";"Niveau";"X";"Y";"N";"Z";"DLA";"Etat";"PA";"PV";"PX";"PXPerso";"PI";"CT";"CARAC"
+            if ( $line =~ /^"(\d*)";"([^"]*)"/ )
+            {
+                $MEMBRES{$1}{'Nom'} = Encode::decode_utf8($2);
+            }
+        }
+        return \%MEMBRES;
+    }
+}
+
+1;
---
+++ docker/lib/GLB/HTML.pm
@@ -0,0 +1,116 @@
+package GLB::HTML;
+
+use lib '/home/gobland-bot/lib/';
+use GLB::GLAPI;
+
+my $begin = <<"START_LOOP";
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+    <head>
+        <title>Sopratrolls</title>
+        <link rel="stylesheet" type="text/css" href="/style/common.css" />
+        <link rel="stylesheet" type="text/css" href="/style/menu.css" />
+        <link rel="stylesheet" type="text/css" href="/style/equipement.css" />
+        <script type="text/javascript" src="/js/common.js"></script>
+        <script type="text/javascript" src="/js/domcollapse.js"></script>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+    </head>
+    <body>
+        <div id="page">
+            <div id="header">
+                <br><br><br><br><br><br><br><br>
+                <div id="nav">
+                    <ul id="menu">
+                        <li><a href="index.html" title="Page d'accueil">Accueil</a></li>
+                        <li><a href="#">Consulter</a>
+                            <ul>
+                                <li><a href="equipement.html" title="Equipement des Gob' du Clan">Equipement du Clan</a></li>
+                            </ul>
+                       </li>
+                    </ul>
+                </div>
+            </div>
+START_LOOP
+
+my $end   = <<"END_LOOP";
+            </div>
+        </div>
+    </body>
+</html>
+END_LOOP
+
+sub createIndex {
+    my $filename = '/var/www/localhost/htdocs/index.html';
+    open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
+    binmode($fh, ":utf8");
+
+    print $fh $begin;
+    print $fh $end;
+}
+
+sub createEquipement {
+
+    use POSIX qw(strftime);
+
+    my $filename = '/var/www/localhost/htdocs/equipement.html';
+    open(my $fh, '>', $filename) or die "Could not open file '$filename' $!";
+    binmode($fh, ":utf8");
+    print $fh $begin;
+
+    print $fh '            <div id="content">'."\n";
+    print $fh '                <h1>Possessions</h1>'."\n";
+    print $fh '                <h2 class="expanded">Equipements Gobelins</h2>'."\n";
+    print $fh '                <table cellspacing="0" id="profilInfos">'."\n";
+
+    my $yaml      = '/home/gobland-bot/gl-config.yaml';
+    my $stuff_ref = GLB::GLAPI::GetClanEquipement($yaml);
+    my %stuff     = %{$stuff_ref};
+    my $gobs_ref  = GLB::GLAPI::GetClanMembres($yaml);
+    my %gobs      = %{$gobs_ref};
+
+    for my $gob_id ( sort keys %stuff )
+    {
+        my $date = strftime "%d/%m/%y %H:%M:%S", localtime;
+        print $fh '                    <tr class="expanded">'."\n";
+        print $fh '                        <th>Equipement(s) de '.$gobs{$gob_id}{'Nom'}.' ('.$gob_id.') '."<em>[maj : $date]</em></th>\n";
+        print $fh '                    </tr>'."\n";
+        print $fh '                    <tr>'."\n";
+        print $fh '                        <td>'."\n";
+        print $fh '                            <ul class="membreEquipementList">'."\n";
+        foreach my $e ( sort keys %{$stuff{$gob_id}} )
+        {
+            for my $item_id ( sort keys %{$stuff{$gob_id}{$e}} )
+            {
+                my $min = '';
+                my $desc = Encode::decode_utf8('<b>Non identifié</b>');
+                my $equipe = $e;
+                my $type = '';
+                my $nom = $stuff{$gob_id}{$e}{$item_id}{'Nom'};
+
+                if ( $stuff{$gob_id}{$e}{$item_id}{'Poids'} )
+                {
+                    $min = ', '.$stuff{$gob_id}{$e}{$item_id}{'Poids'}/60 . ' min';
+                }
+                if ( $stuff{$gob_id}{$e}{$item_id}{'Identifie'} eq 'VRAI' )
+                {
+                    $desc = Encode::decode_utf8($stuff{$gob_id}{$e}{$item_id}{'Desc'});
+                }
+                if ( $stuff{$gob_id}{$e}{$item_id}{'Type'} )
+                {
+                    $type = Encode::decode_utf8($stuff{$gob_id}{$e}{$item_id}{'Type'});
+                }
+                print $fh ' ' x 32, '<li class="equipement'.$equipe.'">['.$item_id.'] '.$type.' : '.$nom.' ('.$desc.')'.$min.'</li>'."\n";
+            }
+            if ( $e eq 'Equipe' ) { print $fh '                                <br>'."\n" }
+        }
+        print $fh '                            </ul>'."\n";
+        print $fh '                        </td>'."\n";
+        print $fh '                    </tr>'."\n";
+    }
+
+    print $fh '                </table>'."\n";
+    print $fh $end;
+    close $fh;
+}
+
+1;
