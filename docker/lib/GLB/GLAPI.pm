1bb784b727328b42bb538c42db622f382a079726
lordslair <rvandepo+github@gmail.com>
2018-08-06 14:40:27 +0000

    Added GLB::GetClanMembres2

--- docker/lib/GLB/GLAPI.pm
+++ docker/lib/GLB/GLAPI.pm
@@ -88,4 +88,42 @@ sub GetClanMembres
     }
 }
 
+sub GetClanMembres2
+{
+    my $glfile = shift;
+    my $glyaml = YAML::Tiny->read( $glfile );
+    my %MEMBRES;
+
+    my $browser = new LWP::UserAgent;
+    my $request = new HTTP::Request( GET => "http://ie.gobland.fr/IE_ClanMembres2?id=$glyaml->[0]{gl_user}&passwd=$glyaml->[0]{gl_api_key}" );
+    my $headers = $request->headers();
+       $headers->header( 'User-Agent','Mozilla/5.0 (compatible; Konqueror/3.4; Linux) KHTML/3.4.2 (like Gecko)');
+       $headers->header( 'Accept', 'text/html, image/jpeg, image/png, text/*, image/*, */*');
+       $headers->header( 'Accept-Encoding','x-gzip, x-deflate, gzip, deflate');
+       $headers->header( 'Accept-Charset', 'iso-8859-15, utf-8;q=0.5, *;q=0.5');
+       $headers->header( 'Accept-Language', 'fr, en');
+       $headers->header( 'Referer', 'http://ie.gobland.fr');
+    my $response = $browser->request($request);
+
+    if ($response->is_success)
+    {
+        foreach my $line (split(/\n/,$response->content))
+        {
+            chomp ($line);
+            #"Id";"Nom";"DLA";"BPDLA";"BMDLA";"PVMax";"BPPVMax";"BMPVMax";"ATT";"BPATT";"BMATT";"ESQ";"BPESQ";"BMESQ";"DEG";"BPDEG";"BMDEG";
+            #           "REG";"BPREG";"BMREG";"PER";"BPPER";"BMPER";"BPArm";"BMArm";"PITotal";
+            #"Faim";
+            #"MM";"BMM";"BPMM";"BMMM";"RM";"BRM";"BPRM";"BMRM";"MT";"BMT";"BPMT";"BMMT";"RT";"BRT";"BPRT";"BMRT";
+            #"MR";"BMR";"BPMR";"BMMR";"RR";"BRR";"BPRR";"BMRR";"MS";"BMS";"BPMS";"BMMS";"RS";"BRS";"BPRS";"BMRS";
+            #"MC";"BMC";"BPMC";"BMMC";"RC";"BRC";"BPRC";"BMRC";"MP";"BMP";"BPMP";"BMMP";"RP";"BRP";"BPRP";"BMRP"
+            if ( $line =~ /^"(\d*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)";"([^"]*)"/ )
+            {
+                $MEMBRES{$1}{'PVMax'} = $6;
+                $MEMBRES{$1}{'PITotal'} = Encode::decode_utf8($26);
+            }
+        }
+        return \%MEMBRES;
+    }
+}
+
 1;
