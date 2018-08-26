55180213a816cf9f68a31c2d06c764eeb19029ac
lordslair <rvandepo+github@gmail.com>
2018-08-26 11:50:10 +0000

    Modified <title> to be dynamic from YAML

--- docker/lib/GLB/functions.pm
+++ docker/lib/GLB/functions.pm
@@ -3,11 +3,16 @@ package GLB::functions;
 use warnings;
 use strict;
 
+use YAML::Tiny;
+my $yaml      = '/home/gobland-bot/gl-config.yaml';
+my $glyaml    = YAML::Tiny->read( $yaml );
+my $clan_name = $glyaml->[0]{gl_clan_name};
+
 our $begin = <<"START_LOOP";
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
     <head>
-        <title>Rabatteurs de Khaket</title>
+        <title>#CLAN_NAME#</title>
         <link rel="stylesheet" type="text/css" href="/style/common.css" />
         <link rel="stylesheet" type="text/css" href="/style/menu.css" />
         <link rel="stylesheet" type="text/css" href="/style/equipement.css" />
@@ -22,18 +27,18 @@ our $begin = <<"START_LOOP";
                 <br><br><br><br><br><br><br><br>
                 <div id="nav">
                     <ul id="menu">
-                        <li><a href="http://rabatteurs.lordslair.net/index.html" title="Page d'accueil">Accueil</a></li>
+                        <li><a href="/index.html" title="Page d'accueil">Accueil</a></li>
                         <li><a href="#">Consulter</a>
                             <ul>
-                                <li><a href="http://rabatteurs.lordslair.net/equipement.html" title="Equipement des Gob' du Clan">Equipement du Clan</a></li>
-                                <li><a href="http://rabatteurs.lordslair.net/materiaux.html" title="Materiaux des Gob' du Clan">Materiaux du Clan</a></li>
-                                <li><a href="http://rabatteurs.lordslair.net/composants.html" title="Composants des Gob' du Clan">Composants du Clan</a></li>
+                                <li><a href="/equipement.html" title="Equipement des Gob' du Clan">Equipement du Clan</a></li>
+                                <li><a href="/materiaux.html" title="Materiaux des Gob' du Clan">Materiaux du Clan</a></li>
+                                <li><a href="/composants.html" title="Composants des Gob' du Clan">Composants du Clan</a></li>
                             </ul>
                        </li>
                         <li><a href="" title="">Outils</a>
                             <ul>
-                                <li><a href="http://rabatteurs.lordslair.net/pxbank.html" title="PX Bank du Clan">PX Bank</a></li>
-                                <li><a href="http://rabatteurs.lordslair.net/GPS.html" title="GPS">GPS</a></li>
+                                <li><a href="/pxbank.html" title="PX Bank du Clan">PX Bank</a></li>
+                                <li><a href="/GPS.html" title="GPS">GPS</a></li>
                             </ul>
                         </li>
                         <li><a href="" title="">Liens</a></li>
@@ -41,6 +46,8 @@ our $begin = <<"START_LOOP";
                 </div>
             </div>
 START_LOOP
+
+$begin =~ s/#CLAN_NAME#/$clan_name/;
 
 our $end   = <<"END_LOOP";
             </div>
