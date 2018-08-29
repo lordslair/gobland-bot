bfa55f31571e36f6b5eeae978a32475e183fb2f8
lordslair <rvandepo+github@gmail.com>
2018-08-29 15:44:24 +0000

    Simplified JS call in /GPS

--- docker/lib/GLB/variables.pm
+++ docker/lib/GLB/variables.pm
@@ -113,21 +113,6 @@ function sortTable(n) {
 </script>
 SORT_SCRIPT
 
-our $vuescript = <<VUE_SCRIPT;
-<script>
-function showTooltip(evt, text) {
-          let tooltip = document.getElementById("tooltip");
-          tooltip.innerHTML = text;
-          tooltip.style.display = "block";
-          tooltip.style.left = evt.pageX + 10 + 'px';
-          tooltip.style.top = evt.pageY + 10 + 'px';
-}
-
-function hideTooltip() {
-          var tooltip = document.getElementById("tooltip");
-          tooltip.style.display = "none";
-}
-</script>
-VUE_SCRIPT
+our $vuescript = '      <script type="text/javascript" src="/js/tt-gps.js"></script>'."\n";
 
 1;
