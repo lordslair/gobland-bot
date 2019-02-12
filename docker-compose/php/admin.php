93b5e368531c511ac8a2f2e492fae6d28620c433
lordslair <rvandepo+github@gmail.com>
2019-02-12 14:04:37 +0000

    Added include for head/header

--- docker-compose/php/admin.php
+++ docker-compose/php/admin.php
@@ -1,47 +1,12 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html>
   <head>
-<?php
-    print ('<title>Gobland-IT ('.$_ENV["CLANID"].') '.$_ENV["CLANNAME"].'</title>');
-?>
-    <link rel="stylesheet" type="text/css" href="/style/common.css" />
-    <link rel="stylesheet" type="text/css" href="/style/menu.css" />
-    <link rel="stylesheet" type="text/css" href="/style/tt_r.css" />
-    <script type="text/javascript" src="/js/common.js"></script>
-    <script type="text/javascript" src="/js/domcollapse.js"></script>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+<?php include 'inc.head.php'; ?>
   </head>
   <body>
     <div id="page">
       <div id="header">
-        <br><br><br><br><br><br><br><br>
-        <div id="nav">
-          <ul id="menu">
-            <li><a href="/index.php" title="Page d'accueil">Accueil</a></li>
-            <li><a href="#">Consulter</a>
-              <ul>
-                <li><a href="/equipements.php" title="Équipement des Gob' du Clan">Équipement du Clan</a></li>
-                <li><a href="/materiaux.php" title="Matériaux des Gob' du Clan">Matériaux du Clan</a></li>
-                <li><a href="/composants.php" title="Composants des Gob' du Clan">Composants du Clan</a></li>
-                <li><a href="/cavernes.php" title="Cavernes du Clan">Cavernes du Clan</a></li>
-              </ul>
-            </li>
-            <li><a href="" title="">Outils</a>
-              <ul>
-                <li><a href="/pxbank.php" title="PX Bank du Clan">PX Bank</a></li>
-                <li><a href="/gps.php" title="GPS">GPS</a></li>
-                <li><a href="/radar.php" title="Radar">Radar</a></li>
-                <li><a href="/cdm.php" title="CdM">CdM Collector</a></li>
-              </ul>
-            </li>
-            <li><a href="" title="">Liens</a>
-              <ul>
-                <li><a href="/admin.php" title="Admin">Admin</a></li>
-                <li><a href="/locator.php" title="Locator">Locator</a></li>
-              </ul>
-            </li>
-          </ul>
-        </div>
+<?php include 'inc.header.php'; ?>
       </div>
       <div id="content">
         <h1>Page d'administration</h1>
