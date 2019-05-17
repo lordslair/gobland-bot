f969f517f4c637c89e2e8dce75a928a7df5d2b71
lordslair <rvandepo+github@gmail.com>
2019-05-17 17:07:20 +0000

    Added changelog.php

---
+++ docker-compose/php/changelog.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html>
+  <head>
+<?php include 'inc.head.php'; ?>
+  </head>
+  <body>
+    <div id="page">
+      <div id="header">
+<?php include 'inc.header.php'; ?>
+      </div>
+      <div id="content">
+        <h1>Changelog</h1>
+        <div id="profilInfos">
+          <fieldset>
+          <legend>Gobland-IT Version 3</legend>
+            <ul>
+              <li>2019-05-17 : Ajout de la page de Changelog (<b>@Omnivore</b>)
+              <li>2019-05-17 : Ajout d'une colonne 'Faim' sur /index.php (<b>@Le Kipu</b>)
+            </ul>
+          </fieldset>
+        </div> <!-- profilInfos -->
+      </div> <!-- content -->
+    </div> <!-- page -->
+  </body>
+</html>
