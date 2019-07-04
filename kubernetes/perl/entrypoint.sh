09fa343e2ec54fa5267e51b1a172d37e8f17bd4a
lordslair <rvandepo+github@gmail.com>
2019-07-04 12:13:00 +0000

    Added entrypoint.sh

---
+++ kubernetes/perl/entrypoint.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+echo "`date +"[%F %X]"` Building Perl dependencies and system set-up ..."
+
+apk update \
+    && apk add --no-cache perl perl-libwww perl-dbi perl-dbd-mysql \
+    && apk add --no-cache --virtual .build-deps \
+                                    curl make perl-dev libc-dev gcc bash tzdata \
+    && curl -L https://cpanmin.us | perl - App::cpanminus --no-wget \
+    && cpanm --no-wget YAML::Tiny \
+                       File::Pid \
+    && cp /usr/share/zoneinfo/Europe/Paris /etc/localtime \
+    && apk del .build-deps \
+    && rm -rf /root/.cpanm
+
+echo "`date +"[%F %X]"` Build done ..."
+
+exec /code/perl/gobland-it
