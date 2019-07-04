84169c92ef6879baf1f69b15b7d441ad4e9c4f39
lordslair <rvandepo+github@gmail.com>
2019-07-04 12:59:49 +0000

    Modified for MariaDB

--- kubernetes/perl/data/initDB.pl
+++ kubernetes/perl/data/initDB.pl
@@ -15,7 +15,7 @@ my $dbh       = DBI->connect($dsn, 'root', $db_pass, { RaiseError => 1 }) or die
 
 foreach my $db (@db_list)
 {
-    print "DB: $db\n";
+    print "[initDB] DB: $db\n";
 
     $dbh->do("CREATE DATABASE IF NOT EXISTS `$db`");
     $dbh->do("USE `$db`");
