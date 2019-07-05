235a44ab936ec4347e540b53da101616d931e1a3
lordslair <rvandepo+github@gmail.com>
2019-07-05 15:00:06 +0000

    Modified for MariaDB

--- kubernetes/perl/setMP2Kills.pl
+++ kubernetes/perl/setMP2Kills.pl
@@ -3,89 +3,79 @@ use strict;
 use warnings;
     
 use DBI;
-use YAML::Tiny;
 use POSIX qw(strftime);
+use Encode;
 
-my $yaml_file = 'master.yaml';
-my $yaml      = YAML::Tiny->read( $yaml_file );
-my @db_list   = @{$yaml->[0]{db_list}};
-my $db_path   = '/db';
-    
+my $logging   = 1;
+
+my @db_list   = split(',', $ENV{'DBLIST'});
+my $db_driver = 'mysql';
+my $db_host   = 'gobland-it-mariadb';
+my $db_port   = '3306';
+my $db_pass   = $ENV{'MARIADB_ROOT_PASSWORD'};
+my $dsn       = "DBI:$db_driver:host=$db_host;port=$db_port";
+my $dbh       = DBI->connect($dsn, 'root', $db_pass, { RaiseError => 1 }) or die $DBI::errstr;
+
 foreach my $db (@db_list)
 {
-    print "DB: $db | setMP2Kills\n";
-    
-    if ( -f "$db_path/$db" )
+    $dbh->do("USE `$db`");
+    logEntry("[setMP2Kills] DB: $db");
+
+    my $now     = strftime "%Y-%m-%d", localtime;
+
+    my $req_mps = $dbh->prepare( "SELECT MPBot.Id,Gobelins.Id,Gobelins.Gobelin,PMSubject,PMDate,PMText \
+                                  FROM Gobelins \
+                                  INNER JOIN MPBot on Gobelins.Id = MPBot.IdGob \
+                                  WHERE ( PMText LIKE '%débarrassé%' OR PMText LIKE '%Son cadavre%' ) \
+                                  AND PMDate LIKE '$now%'");
+    $req_mps->execute();
+
+    while (my @row = $req_mps->fetchrow_array)
     {
-        my $dbh = DBI->connect(
-            "dbi:SQLite:dbname=$db_path/$db",
-            "",
-            "",
-            { RaiseError => 1 },
-        ) or die $DBI::errstr;
+        my $mp_id      = $row[0];
+        my $gob_id     = $row[1];
+        my $gob_name   = $row[2];
+        my $mp_subject = $row[3];
+        my $mp_date    = $row[4];
+        my $mp_text    = $row[5];
 
-        my $now     = strftime "%Y-%m-%d", localtime;
+        my $mob_id;
+        my $mob_name;
 
-        my $req_mps = $dbh->prepare( "SELECT MPBot.Id,Gobelins.Id,Gobelins.Gobelin,PMSubject,PMDate,PMText \
-                                      FROM Gobelins \
-                                      INNER JOIN MPBot on Gobelins.Id = MPBot.IdGob \
-                                      WHERE ( PMText LIKE '%débarrassé%' OR PMText LIKE '%Son cadavre%' ) \
-                                      AND PMDate LIKE '$now%'");
-        $req_mps->execute();
-
-        while (my @row = $req_mps->fetchrow_array)
+        # DATA: Résultat Attaque - Orque (305855)
+        # DATA: Résultat Attaque Suivant - Chauve-souris Géante (303448)
+        if ( $mp_subject =~ / - ([-\P{Latin}\w\s\']*) \((\d*)\)/ )
         {
-            my $mp_id      = $row[0];
-            my $gob_id     = $row[1];
-            my $gob_name   = $row[2];
-            my $mp_subject = $row[3];
-            my $mp_date    = $row[4];
-            my $mp_text    = $row[5];
-
-            my $mob_id;
-            my $mob_name;
-
-            # DATA: Résultat Attaque - Orque (305855)
-            # DATA: Résultat Attaque Suivant - Chauve-souris Géante (303448)
-            if ( $mp_subject =~ / - ([-\P{Latin}\w\s\']*) \((\d*)\)/ )
-            {
-                $mob_name = $1;
-                $mob_id   = $2;
-                $mob_name =~ s/\'/\'\'/g;
-            }
-            # DATA: Résultat Potion
-            elsif ( $mp_subject eq 'Résultat Potion' )
-            {
-                # DATA: sur [312463] Bondin
-                if ( $mp_text =~ /sur \[(\d*)\] ([-\P{Latin}\w\s\']*)\.<P>/ )
-                {
-                    $mob_id   = $1;
-                    $mob_name = $2;
-                }
-            }
-            elsif ( $mp_subject eq 'Résultat Eclair' )
-            {
-                # Too complicated to parse for now
-                next;
-            }
-
-print("$mp_id:$gob_id:$gob_name:$mob_id:$mob_name\n");
-
-            my $sth  = $dbh->prepare( "INSERT OR IGNORE INTO Kills VALUES( '$mp_id'      , \
-                                                                           '$mp_date'    , \
-                                                                           '$mob_id'     , \
-                                                                           '$mob_name'   , \
-                                                                           '$gob_id'     , \
-                                                                           '$gob_name'   , \
-                                                                           \"$mp_subject\" , \
-                                                                           \"$mp_text\"  )" );
-
-            $sth->execute();
-            $sth->finish();
+            $mob_name = $1;
+            $mob_id   = $2;
+            $mob_name =~ s/\'/\'\'/g;
         }
-    }
-    else
-    {
-        print "DB $db_path/$db doesn't exist, doin' nothin' [/!\ Run initDB.pl first]\n";
+        # DATA: Résultat Potion
+        elsif ( $mp_subject eq 'Résultat Potion' )
+        {
+            # DATA: sur [312463] Bondin
+            if ( $mp_text =~ /sur \[(\d*)\] ([-\P{Latin}\w\s\']*)\.<P>/ )
+            {
+                $mob_id   = $1;
+                $mob_name = $2;
+            }
+        }
+        elsif ( $mp_subject eq 'Résultat Eclair' )
+        {
+            # Too complicated to parse for now
+            next;
+        }
+
+        my $sth  = $dbh->prepare( "INSERT IGNORE INTO Kills VALUES( '$mp_id'      , \
+                                                                    '$mp_date'    , \
+                                                                    '$mob_id'     , \
+                                                                    '$mob_name'   , \
+                                                                    '$gob_id'     , \
+                                                                    '$gob_name'   , \
+                                                                    \"$mp_subject\" , \
+                                                                    \"$mp_text\"  )" );
+
+        $sth->execute();
+        $sth->finish();
     }
 }
--- kubernetes/perl/setMP2Suivants.pl
+++ kubernetes/perl/setMP2Suivants.pl
@@ -3,58 +3,51 @@ use strict;
 use warnings;
     
 use DBI;
-use YAML::Tiny;
 use POSIX qw(strftime);
+use Encode;
 
-my $yaml_file = 'master.yaml';
-my $yaml      = YAML::Tiny->read( $yaml_file );
-my @db_list   = @{$yaml->[0]{db_list}};
-my $db_path   = '/db';
-    
+my $logging   = 1;
+
+my @db_list   = split(',', $ENV{'DBLIST'});
+my $db_driver = 'mysql';
+my $db_host   = 'gobland-it-mariadb';
+my $db_port   = '3306';
+my $db_pass   = $ENV{'MARIADB_ROOT_PASSWORD'};
+my $dsn       = "DBI:$db_driver:host=$db_host;port=$db_port";
+my $dbh       = DBI->connect($dsn, 'root', $db_pass, { RaiseError => 1 }) or die $DBI::errstr;
+
 foreach my $db (@db_list)
 {
-    print "DB: $db | setMP2Suivants\n";
-    
-    if ( -f "$db_path/$db" )
+    $dbh->do("USE `$db`");
+    logEntry("[setMP2Suivants] DB: $db");
+
+    my $now     = strftime "%Y-%m-%d", localtime;
+
+    my $req_mps = $dbh->prepare( "SELECT Id,IdGob,PMDate,PMSubject,PMText \
+                                  FROM MPBot \
+                                  WHERE PMSubject LIKE 'Infos Suivant%' AND PMDate LIKE '$now%' \
+                                  ORDER BY PMDate \
+                                  LIMIT 100;" ); # To avoid a slow SELECT as MPBot can be huge
+       $req_mps->execute();
+
+    my %suivants;
+
+    while (my @row = $req_mps->fetchrow_array)
     {
-        my $dbh = DBI->connect(
-            "dbi:SQLite:dbname=$db_path/$db",
-            "",
-            "",
-            { RaiseError => 1 },
-        ) or die $DBI::errstr;
-
-        my $now     = strftime "%Y-%m-%d", localtime;
-        my $req_mps = $dbh->prepare( "SELECT Id,IdGob,PMDate,PMSubject,PMText \
-                                      FROM MPBot \
-                                      WHERE PMSubject LIKE 'Infos Suivant%' AND PMDate LIKE '$now%' \
-                                      ORDER BY PMDate \
-                                      LIMIT 100;" ); # To avoid a slow SELECT as MPBot can be huge
-           $req_mps->execute();
-
-        my %suivants;
-
-        while (my @row = $req_mps->fetchrow_array)
+        if ( $row[3] =~ /Infos Suivant - (.*) \((\d*)\) - / )
         {
-            if ( $row[3] =~ /Infos Suivant - (.*) \((\d*)\) - / )
-            {
-                $suivants{$2}{'Nom'}   = $1;
-                $suivants{$2}{'IdGob'} = $row[1];
-            }
-        }
-
-        foreach my $suivant_id ( sort keys %suivants )
-        {
-            $suivants{$suivant_id}{'Nom'} =~ s/\'/\'\'/g;
-            my $sth  = $dbh->prepare( "INSERT OR IGNORE INTO Suivants VALUES( '$suivant_id', \
-                                                                              '$suivants{$suivant_id}{'IdGob'}', \
-                                                                              '$suivants{$suivant_id}{'Nom'}' )" );
-            $sth->execute();
-            $sth->finish();
+            $suivants{$2}{'Nom'}   = $1;
+            $suivants{$2}{'IdGob'} = $row[1];
         }
     }
-    else
+
+    foreach my $suivant_id ( sort keys %suivants )
     {
-        print "DB $db_path/$db doesn't exist, doin' nothin' [/!\ Run initDB.pl first]\n";
+        $suivants{$suivant_id}{'Nom'} =~ s/\'/\'\'/g;
+        my $sth  = $dbh->prepare( "INSERT IGNORE INTO Suivants VALUES( '$suivant_id', \
+                                                                       '$suivants{$suivant_id}{'IdGob'}', \
+                                                                       '$suivants{$suivant_id}{'Nom'}' )" );
+        $sth->execute();
+        $sth->finish();
     }
 }
