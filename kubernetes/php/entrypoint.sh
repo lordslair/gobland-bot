02a549c03bb1ad32acaf3cc35aae6382f43c8502
lordslair <rvandepo+github@gmail.com>
2019-07-08 13:12:52 +0000

    Modified to use docker-php-* scripts

--- kubernetes/php/entrypoint.sh
+++ kubernetes/php/entrypoint.sh
@@ -3,12 +3,11 @@
 echo "`date +"%F %X"` Building PHP dependencies and system set-up ..."
 
 apk update --no-cache \
-    && apk upgrade --no-cache \
-    && apk add --no-cache php7-mysqli \
     && apk add --no-cache --virtual .build-deps \
                                     tzdata \
     && cp /usr/share/zoneinfo/Europe/Paris /etc/localtime \
-    && apk del .build-deps
+    && apk del .build-deps \
+    && docker-php-ext-install mysqli
 
 echo "`date +"%F %X"` Build done ..."
 
