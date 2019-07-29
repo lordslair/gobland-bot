409aafe648e02c11d80fb1d0ae6c91b59d4c2cc3
lordslair <rvandepo+github@gmail.com>
2019-07-29 16:30:30 +0000

    Added new login features

---
+++ kubernetes/php/reset.php
@@ -0,0 +1,108 @@
+<?php
+include 'inc.session.php';
+
+// Include config file
+require_once "inc.db-global.php";
+
+// Define variables and initialize with empty values
+$new_password = $confirm_password = "";
+$new_password_err = $confirm_password_err = "";
+
+// Processing form data when form is submitted
+if($_SERVER["REQUEST_METHOD"] == "POST"){
+
+    // Validate new password
+    if(empty(trim($_POST["new_password"]))){
+        $new_password_err = "Entrez un nouveau mot de passe";
+    } elseif(strlen(trim($_POST["new_password"])) < 6){
+        $new_password_err = "Le mot de passe doit être >6 caractères";
+    } else{
+        $new_password = trim($_POST["new_password"]);
+    }
+
+    // Validate confirm password
+    if(empty(trim($_POST["confirm_password"]))){
+        $confirm_password_err = "Confirmez le nouveau mot de passe";
+    } else{
+        $confirm_password = trim($_POST["confirm_password"]);
+        if(empty($new_password_err) && ($new_password != $confirm_password)){
+            $confirm_password_err = "Mots de passe non-identiques";
+        }
+    }
+
+    // Check input errors before updating the database
+    if(empty($new_password_err) && empty($confirm_password_err)){
+        // Prepare an update statement
+        $sql = "UPDATE users SET account_password = ? WHERE account_id = ?";
+
+        if($stmt = $db->prepare($sql)){
+            // Bind variables to the prepared statement as parameters
+            $stmt->bind_param("si", $param_password, $param_id);
+
+            // Set parameters
+            $param_password = password_hash($new_password, PASSWORD_DEFAULT);
+            $param_id = $_SESSION["id"];
+
+            // Attempt to execute the prepared statement
+            if($stmt->execute()){
+                // Password updated successfully. Destroy the session, and redirect to login page
+                session_destroy();
+                header("location: login.php");
+                exit();
+            } else{
+                echo "Oops! Something went wrong. Please try again later.";
+            }
+        }
+
+        // Close statement
+        $stmt->close();
+    }
+
+    // Close connection
+    $db->close();
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Modifier le mot de passe</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.css">
+    <style type="text/css">
+        /* Works for smartphones */
+        @media (min-width: 640px) {
+            body { font: 32px sans-serif; }
+            .wrapper{ width: 100%; padding: 20px;margin: auto; }
+        }
+        /* Works for laptops */
+        @media (min-width: 1024px) {
+            body { font: 16px sans-serif; }
+            .wrapper{ width: 350px; padding: 20px;margin: auto; }
+        }
+    </style>
+</head>
+<body>
+    <div class="wrapper">
+        <h2>Modifier votre mot de passe</h2>
+        <h3>User: <?php echo htmlspecialchars($_SESSION["gob_name"]); ?></h3>
+        <p>Merci d'indiquer votre nouveau mot de passe</p>
+        <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+            <div class="form-group <?php echo (!empty($new_password_err)) ? 'has-error' : ''; ?>">
+                <label>Nouveau Password</label>
+                <input type="password" name="new_password" class="form-control" value="<?php echo $new_password; ?>">
+                <span class="help-block"><?php echo $new_password_err; ?></span>
+            </div>
+            <div class="form-group <?php echo (!empty($confirm_password_err)) ? 'has-error' : ''; ?>">
+                <label>Confirmation Password</label>
+                <input type="password" name="confirm_password" class="form-control">
+                <span class="help-block"><?php echo $confirm_password_err; ?></span>
+            </div>
+            <div class="form-group">
+                <input type="submit" class="btn btn-primary" value="Modifier">
+                <a class="btn btn-link" href="index.php">Annuler</a>
+            </div>
+        </form>
+    </div>
+</body>
+</html>
