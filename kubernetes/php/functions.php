03544986170a23511d7fd39f2b39dc44e3daf5e5
lordslair <rvandepo+github@gmail.com>
2019-09-23 13:01:40 +0000

    Added Minerai de Titane

--- kubernetes/php/functions.php
+++ kubernetes/php/functions.php
@@ -140,6 +140,7 @@ function GetMateriauIcon($nom)
     elseif ( $nom == 'Minerai de Mithril' )    { $png = '<img src="/images/stuff/icon_1070.png" title="'.$nom.'">'; }
     elseif ( $nom == 'Minerai d\'Adamantium' ) { $png = '<img src="/images/stuff/icon_1071.png" title="'.$nom.'">'; }
     elseif ( $nom == 'Minerai de Fer' )        { $png = '<img src="/images/stuff/icon_1072.png" title="'.$nom.'">'; }
+    elseif ( $nom == 'Minerai de Titane' )     { $png = '<img src="/images/stuff/icon_1079.png" title="'.$nom.'">'; }
     elseif ( $nom == 'Cuir' )                  { $png = '<img src="/images/stuff/icon_98.png"   title="'.$nom.'">'; }
     elseif ( $nom == 'Tissu' )                 { $png = '<img src="/images/stuff/icon_103.png"  title="'.$nom.'">'; }
     elseif ( $nom == 'Pierre' )                { $png = '<img src="/images/stuff/icon_1142.png" title="'.$nom.'">'; }
